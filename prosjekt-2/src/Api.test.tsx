a39885b4df2ac2363805a7c762079b91e6aeaf00
Richard Alexander Haydon <richarah@stud.ntnu.no>
2022-09-30 13:22:20 +0200

    Skeleton syntax for API test

---
+++ prosjekt-2/src/Api.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {expect, jest, test} from '@jest/globals';
+import Api from './Api';
+
+/** Skeleton syntax, to be updated with actual tests*/
+
+/**
+test(input, () => {
+  expect(method()).result('Hei');
+});
+*/
