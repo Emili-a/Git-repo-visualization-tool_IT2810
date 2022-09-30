bad07e5314adaba656fedf380fcacbaef18d4239
kbrattli <kbrattli@live.no>
2022-09-30 15:27:25 +0200

    Login page @kennbr

--- prosjekt-2/src/Login.tsx
+++ prosjekt-2/src/Login.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
 
 function Login() {
   return (
     <div className="Login">
       <h1>Sjekk info om git repo</h1>
-      <input id="url" type="text" placeholder="Skriv inn repo navn" />
-      <input id="token" type="text" placeholder="Skriv inn token" />
+      <input id="id" type="text" placeholder="Skriv inn prosjekt-id til repo"/>
+      <input id="token" type="text" placeholder="Skriv inn token"/>
       <button onClick={checkRepo}>Sjekk repo</button>
     </div>
   );
 }
 
 interface gitrepo {
-  url: string;
+  id: string;
   token: string;
 }
 
-function checkRepo() {
-  const url = document.getElementById("url") as HTMLInputElement;
+async function checkRepo() {
+  const id = document.getElementById("url") as HTMLInputElement;
   const token = document.getElementById("token") as HTMLInputElement;
   const repo: gitrepo = {
-    url: url.value,
+    id: id.value,
     token: token.value
   }
-  console.log(repo);
-  const api = axios.create({
-    baseURL: url.value
-  })
-// check if repo exist with react ajax
-  api.get('/').then((response) => {
-    console.log(response);
-  }).catch((error) => {
-    console.log(error);
-  })
+  const url = "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" + repo.id + "repository/branches";
+  const response = await fetch(url, { method: 'GET', headers: { 'PRIVATE-TOKEN': repo.token } });
+  const status = response.status;
+
+  if (status === 200) {
+    console.log("Repo funnet");
+  } 
+  
+  else {
+    alert("prosjekt-id eller token er feil");
+  }
+  
 }
 
 export default Login;
