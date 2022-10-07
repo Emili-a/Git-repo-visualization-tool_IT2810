2b1988b1264202af61dece082bdac2a63fe2e45e
Richard Alexander Haydon <richarah@stud.ntnu.no>
2022-10-07 19:27:51 +0200

    Fixed config

--- prosjekt-2/babel.config.js
+++ prosjekt-2/babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  transform: {},
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',
   ],
+};
+
+module.exports = api => {
+  const isTest = api.env('test');
+  return {};
 };
